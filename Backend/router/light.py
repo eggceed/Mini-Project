0a60b89e7fe0f5bfd3186920beb177f4116a93f6
karnse <suphawit.se@ku.th>
2023-02-08 13:13:49 +0700

    seperate type

--- Backend/router/light.py
+++ Backend/router/light.py
@@ -6,11 +6,13 @@ from typing import Union,Optional,List
 
 class Room(BaseModel):
     id:int
-    name:str = '' 
-    type:str = 'web' #web or device
+    powered:Union[bool,None] = None
+    isAuto:Union[bool,None] = None
+    name:str = ''
     intensity:int=100
-    powered:bool=False
-    isAuto:bool=False
+
+class Req_dep(BaseModel):
+    type:str = ''
 
 router = APIRouter(prefix='/light')
 
@@ -26,16 +28,22 @@ def view_roomdata():
     return list(collection.find({},{'_id':0}))
 
 @router.put('/')
-def update_roomdata(room:Room):
+def update_roomdata(room:Room,req_dep:Req_dep):
     if(room.id < 1 or room.id > 4):
         raise HTTPException(status_code=400,detail="Room id is invalid")
-    if room.type=='device':
+    if req_dep.type=='device':
         collection.update_one({'id':room.id},{'$set':{'powered':room.powered}})
-    elif(room.type=='web'):
-        if(room.intensity < 0 or room.intensity > 100):
+    elif(req_dep.type=='web'):
+        if(room.intensity < 0 or room.intensity > 100 ):
             raise HTTPException(status_code=400,detail="Intensity is invalid")
         if(room.name == ''):
             raise HTTPException(status_code=400,detail="Room name is invalid")
+        if(room.powered == None):
+            raise HTTPException(status_code=400,detail='powered is empty')
+        if(room.isAuto == None):
+            raise HTTPException(status_code=400,detail='isAuto is empty')
         collection.update_one({'id':room.id},{'$set':{'intensity':room.intensity,'isAuto':room.isAuto,'powered':room.powered}})
+    else:
+        raise HTTPException(status_code=400,detail="type is invalid")
     return {"msg":"success"}
 
